8ab0a502207605a8b0bf613dddb6a29a3f2d0fb2
rituraj roj <riturajroj1234@gmail.com>
2024-04-25 18:42:08 +0530

    add Employee function

--- src/main/java/com/slash/springboot/employeeapi/controller/EmployeeController.java
+++ src/main/java/com/slash/springboot/employeeapi/controller/EmployeeController.java
@@ -3,9 +3,13 @@ package com.slash.springboot.employeeapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.slash.springboot.employeeapi.dto.Employee;
@@ -31,5 +35,11 @@ public class EmployeeController {
     @GetMapping("/employees/{empId}")
     public Employee getEmployeeById(@PathVariable int empId) {
         return empServiceRef.getEmployeeById(empId);
+    }
+
+    // Method to add employee
+    @PostMapping(value = "employees", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public void addEmployee(@RequestBody Employee employee){
+        empServiceRef.addEmployee(employee);
     }
 }
